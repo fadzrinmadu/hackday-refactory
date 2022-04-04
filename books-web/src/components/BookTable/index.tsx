9b0a90ede189a1b5b40556c1b6c00b12e865e0b3
fadzrinmadu <muhfadzrinmadu@gmail.com>
2022-04-04 16:38:18 +0700

    Add callback when book data is empty

--- books-web/src/components/BookTable/index.tsx
+++ books-web/src/components/BookTable/index.tsx
@@ -32,37 +32,44 @@ export default function BookTable() {
         <Message variant={message.type} text={message.text} />
       )}
 
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th style={{ width: '3%' }}>No</th>
-            <th style={{ width: '10%' }}>ISBN</th>
-            <th>Book Name</th>
-            <th>Author</th>
-            <th style={{ width: '30%' }}>Summary</th>
-            <th>Year</th>
-            <th>Publisher</th>
-            <th className="text-center" style={{ width: '10%' }}>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {books.length > 0 && books.map((book: BookTypes, index) => (
-            <Row
-              key={book.id}
-              rowNumber={++index}
-              bookId={book.id}
-              bookIsbn={book.isbn}
-              bookName={book.name}
-              bookAuthor={book.author}
-              bookSummary={book.summary}
-              bookYear={book.year}
-              bookPublisher={book.publisher}
-              fetchAllBooks={fetchAllBooks}
-              setMessage={setMessage}
-            />
-          ))}
-        </tbody>
-      </Table>
+      {books.length > 0 ? (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th style={{ width: '3%' }}>No</th>
+              <th style={{ width: '10%' }}>ISBN</th>
+              <th>Book Name</th>
+              <th>Author</th>
+              <th style={{ width: '30%' }}>Summary</th>
+              <th>Year</th>
+              <th>Publisher</th>
+              <th className="text-center" style={{ width: '10%' }}>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.length > 0 && books.map((book: BookTypes, index) => (
+              <Row
+                key={book.id}
+                rowNumber={++index}
+                bookId={book.id}
+                bookIsbn={book.isbn}
+                bookName={book.name}
+                bookAuthor={book.author}
+                bookSummary={book.summary}
+                bookYear={book.year}
+                bookPublisher={book.publisher}
+                fetchAllBooks={fetchAllBooks}
+                setMessage={setMessage}
+              />
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <Message
+          variant="info"
+          text="Book data is still empty, please add new data"
+        />
+      )}
     </>
   );
 }
